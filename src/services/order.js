7daf6f22cdee0a231bde7ad16934a39e09de0988
Yekinni Ajibola <yekinnijibola@gmail.com>
2022-05-31 08:52:14 +0100

    feat: added create order service

--- src/services/order.js
+++ src/services/order.js
@@ -1,3 +1,14 @@
-const orderService = () => {};
+const orderService = async (data) => {
+  const response = await fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).catch((error) => console.log(error));
+
+  if (response) {
+    const result = response.json();
+    return result;
+  }
+};
 
 export default orderService;
