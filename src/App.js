f3d4d1311d850eea29ab80240fdea1d26e772a37
Yekinni Ajibola <yekinnijibola@gmail.com>
2022-06-08 09:54:11 +0100

    refactor: removed admin page and route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Order from "./pages/Order";
 import Confirm from "./pages/Confirm";
-import Admin from "./pages/Admin";
 
 // const theme = {
 //   global: {
@@ -23,7 +22,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/order" element={<Order />} />
           <Route path="/confirmation" element={<Confirm />} />
-          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </Grommet>
